b7f62450e7efb4dad03c3a3cf4f67459c1734c46
kecca9742@gmail.com <kecca9742@gmail.com>
2023-05-16 17:41:28 +0900

    7일차

---
+++ Study01/Chap01/main.js
@@ -0,0 +1,23 @@
+let currencyRatio = {
+    USD:{
+        KRW:1,335.11
+        USD:1
+        VND:23,460.00
+        unit:"달러"
+    },
+    KRW:{
+        KRW:1,
+        USD:0.00075,
+        VND:17.57,
+        unit:"원"
+    },
+    VND:{
+        KRW:0.057,
+        USD:0.000043,
+        VND:1
+        unit:"동"
+    }
+}
+  //console.log(currencyRatio.USD.unit)
+ // console.log(currencyRatio[])
+
